55e635f82283f04397ce1f65ec7ab35e679b3b18
GG <drcybfreeman@gmail.com>
2024-02-23 11:18:14 +0600

    upd

--- src/main/java/u6/multi_thread/s0/synch/consumer_producer_shared_resource/ConsumerProducerApp.java
+++ src/main/java/u6/multi_thread/s0/synch/consumer_producer_shared_resource/ConsumerProducerApp.java
@@ -2,29 +2,14 @@ package u6.multi_thread.s0.synch.consumer_producer_shared_resource;
 
 public class ConsumerProducerApp {
     public static void main(String[] args) {
-        Thread t1 = null;
-        Thread t2 = null;
-
         Resource resource = new Resource();
 
-//        ProducerMyRunnable p = new ProducerMyRunnable(resource);
-//        t1 = new Thread(p);
-        t1 = new Thread(() -> {
-            for (int i = 0; i < 5; ++i) {
-                resource.put(i);
-            }
-        });
-
-//        ConsumerMyRunnable c = new ConsumerMyRunnable(resource);
-//        t2 = new Thread(c);
-        t2 = new Thread(() -> {
-            for (int i = 0; i < 5; ++i) {
-                resource.get();
-            }
-        });
+        Thread t1 = new Thread(new ConsumerMyRunnable(resource));
+        Thread t2 = new Thread(new ProducerMyRunnable(resource));
 
         t1.start();
         t2.start();
+
         try {
             t1.join();
             t2.join();
