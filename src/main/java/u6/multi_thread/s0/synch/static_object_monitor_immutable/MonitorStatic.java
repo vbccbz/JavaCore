07b4469a4d2d2bd51be02a87787e1a25599250c4
GG <drcybfreeman@gmail.com>
2024-02-27 15:47:48 +0600

    upd

--- src/main/java/u6/multi_thread/s0/synch/static_object_monitor_immutable/MonitorStatic.java
+++ src/main/java/u6/multi_thread/s0/synch/static_object_monitor_immutable/MonitorStatic.java
@@ -1,47 +1,47 @@
 package u6.multi_thread.s0.synch.static_object_monitor_immutable;
 
 public class MonitorStatic {
-    private static Integer monitor = 0;
-    /*
-    * Here are 3 monitor. Integer is the immutable. If you've created Integer - you can't change it.
-    * The value inside Integer is final. It's the same as with String. Have changed -> have created new object.
-    * After first monitor++; the monitor in MonitorStatic will be another object.
-    */
+    private static Integer monitor1 = 0;
+    private static Integer monitor2 = 0;// the same object in memory as monitor :D so, do not do that
+
     public static void main(String[] args) {
-        new Thread(()->{
-            synchronized (monitor){
+        new Thread(() -> {
+            System.out.println("1 before synch");
+            synchronized (monitor1) {
                 System.out.println("1");
-                monitor++;// monitor = monitor + 1;
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(10000);
+                } catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                }
+                monitor1++;
+                /* Integer is the immutable. The value inside Integer is final. It is similar to String.
+                If you've created Integer - you can't change it, but can replace.
+                After ++, monitor1 will refere to another object.
+                If Thread2 has time to connect to old monitor1 - he will be attached to old monitor1.
+                If next threads will sync on monitor - they will infer with previous threads.
+                */
+
+                System.out.println("1 changed monitor");
+                try {
+                    Thread.sleep(10000);
                 } catch (InterruptedException exception) {
                     exception.printStackTrace();
                 }
                 System.out.println("!1");
             }
         }).start();
-        new Thread(()->{
-            synchronized (monitor){
+
+        new Thread(() -> {
+            System.out.println("2 before synch");
+            synchronized (monitor1) {
                 System.out.println("2");
-                monitor++;
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(100);
                 } catch (InterruptedException exception) {
                     exception.printStackTrace();
                 }
                 System.out.println("!2");
-            }
-        }).start();
-        new Thread(()->{
-            synchronized (monitor){
-                System.out.println("3");
-                monitor++;
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException exception) {
-                    exception.printStackTrace();
-                }
-                System.out.println("!3");
             }
         }).start();
     }
