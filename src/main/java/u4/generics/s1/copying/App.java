60a9474062e65bd5464a50c2acc7cbc300ba75bd
GG <drcybfreeman@gmail.com>
2024-04-13 19:28:10 +0500

    upd

--- src/main/java/u4/generics/s1/copying/App.java
+++ src/main/java/u4/generics/s1/copying/App.java
@@ -2,7 +2,7 @@ package u4.generics.s1.copying;
 
 public class App {
 
-    public static <T> void copy(Container<? super T> dst, Container<? extends T> src) {
+    public static <T> void copy(Container<? extends T> src, Container<? super T> dst) {
     }
 
     public static void main(String[] args) {
@@ -12,9 +12,9 @@ public class App {
         System.out.println(shapes.getClass().getName());
         System.out.println(circles.getClass().getName());
 
-//!        App.<Shape>copy(circles, shapes);
-//!        App.<Circle>copy(circles, shapes);
-//!        copy(circles, shapes);
-        copy(shapes, circles);
+        App.<Shape>copy(circles, shapes);
+        App.<Circle>copy(circles, shapes);
+        //!copy(shapes, circles);
+        copy(circles, shapes);
     }
 }
