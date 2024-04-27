5f5bab3923858ff6ed67231c8b29d101fac0830e
GG <drcybfreeman@gmail.com>
2024-04-27 15:59:53 +0500

    upd

--- src/main/java/u4/generics/s0/MyGen.java
+++ src/main/java/u4/generics/s0/MyGen.java
@@ -9,41 +9,39 @@ func(123);
 
 
 public class MyGen<T> {
-/*
-4.4 Type Variables
-    A type variable is an unqualified identifier used as a type in class, interface, method,
-    and constructor bodies.
-    A type variable is introduced by the declaration of a type parameter of a generic
-    class, interface, method, or constructor (§8.1.2, §9.1.2, §8.4.4, §8.8.4).
-    TypeParameter:
-    {TypeParameterModifier} Identifier [TypeBound]
-
-4.5 Parameterized Types
-    A class or interface declaration that is generic (§8.1.2, §9.1.2) defines a set of
-    parameterized types.
-    A parameterized type is a class or interface type of the form C<T1,...,Tn>, where C
-    is the name of a generic type and <T1,...,Tn> is a list of type arguments that denote
-    a particular parameterization of the generic type.
-
-8.1.2 Generic Classes and Type Parameters
+    /*
+    public class MyGen<T>{ }
+    is a generic type declaration (it is compiled once).
     A class is generic if it declares one or more type variables (§4.4).
-    These type variables are known as the type parameters of the class. The type
-    parameter section follows the class name and is delimited by angle brackets.
-    TypeParameters:
-< TypeParameterList >
-    TypeParameterList:
-    TypeParameter {, TypeParameter}
-*/
-/*
-<T> is the formal type parameter section.
-This is a declaration of the formal type parameter T ( type variables, параметр типа, тИповый параметр).
-A type variable is an unqualified identifier used as a type in class, interface, static and non-static methods, and constructor bodies.
-*/
-/*
-public class MyGen<T>{ }
-is a generic type declaration (it is compiled once).
-A class is generic if it declares one or more type variables (§4.4).
-*/
+
+    <T> is the formal type parameter section.
+    This is a declaration of the formal type parameter T ( type variables, параметр типа, тИповый параметр).
+    A type variable is an unqualified identifier used as a type in class, interface, static and non-static methods, and constructor bodies.
+
+    4.4 Type Variables
+        A type variable is an unqualified identifier used as a type in class, interface, method,
+        and constructor bodies.
+        A type variable is introduced by the declaration of a type parameter of a generic
+        class, interface, method, or constructor (§8.1.2, §9.1.2, §8.4.4, §8.8.4).
+        TypeParameter:
+        {TypeParameterModifier} Identifier [TypeBound]
+
+    4.5 Parameterized Types
+        A class or interface declaration that is generic (§8.1.2, §9.1.2) defines a set of
+        parameterized types.
+        A parameterized type is a class or interface type of the form C<T1,...,Tn>, where C
+        is the name of a generic type and <T1,...,Tn> is a list of type arguments that denote
+        a particular parameterization of the generic type.
+
+    8.1.2 Generic Classes and Type Parameters
+        A class is generic if it declares one or more type variables (§4.4).
+        These type variables are known as the type parameters of the class. The type
+        parameter section follows the class name and is delimited by angle brackets.
+        TypeParameters:
+    < TypeParameterList >
+        TypeParameterList:
+        TypeParameter {, TypeParameter}
+    */
     T data;
 
     public void add(T data) {
