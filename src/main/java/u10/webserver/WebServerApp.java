0b56821f1cd8a1554507a856334b943cb25709dd
GG <drcybfreeman@gmail.com>
2024-06-23 12:18:46 +0500

    clear buffer...

--- src/main/java/u10/webserver/WebServerApp.java
+++ src/main/java/u10/webserver/WebServerApp.java
@@ -7,26 +7,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WebServerApp {
-  private static List<String> buffer = new ArrayList<String>();
+  private static List<String> browserRequestBuffer = new ArrayList<String>();
+  private static int counter = 0;
 
   public static void readFromBrowser(Socket connector) throws IOException {
     var bufferedReader = new BufferedReader(new InputStreamReader(connector.getInputStream()));
     String str = null;
+    String flag = null;
+    int lng = 0;
     while ((str = bufferedReader.readLine()) != null) {
-
-      if (str.isEmpty()) {
+      if (str.contains("GET")) {
+        flag = "GET";
+      } else if (str.contains("POST")) {
+        flag = "POST";
+      }
+      if (str.contains("Content-Length:")){
+        String lengthInString = str.substring("Content-Length:".length()).trim();
+        lng = Integer.parseInt(lengthInString);
+      }
+      if (str.isEmpty() && flag.equals("GET")) {
         break;
       }
-      buffer.add(str);
+      if (str.isEmpty() && flag.equals("POST")) {
+        char[] msg = new char[lng];
+        bufferedReader.read(msg,0,lng);
+        String s = String.valueOf(msg);
+        browserRequestBuffer.add(s);
+        break;
+      }
+      browserRequestBuffer.add(str);
     }
   }
 
   public static void logBufferToFile() throws Exception {
     File file = new File("data/log.txt");
     PrintWriter printWriter = new PrintWriter(new FileWriter(file, true));
-    printWriter.println("\nNEW LOG!");
-    buffer.stream().forEach(printWriter::println);
+    printWriter.println("NEW LOG " + counter);
+    ++counter;
+    browserRequestBuffer.stream().forEach(printWriter::println);
+    printWriter.println();
     printWriter.flush();
+
   }
 
   public static void sendToBrowserOk(Socket connector) throws Exception {
@@ -44,7 +65,29 @@ public class WebServerApp {
     printWriter.println("Content-Type: text/html; charset=utf-8");//version; answer code;
     printWriter.println();
     printWriter.println("<p>sad:0</p>");
-    printWriter.println("<html><head></head><body><:-(</p</body></html>");
+    printWriter.println("<html><head></head><body><p>:-(</p</body></html>");
+    printWriter.flush();
+  }
+
+  public static void sendToBrowserForm(Socket connector) throws Exception {
+    PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(connector.getOutputStream()));
+    printWriter.println("HTTP/1.1 200 OK");//version; answer code;
+    printWriter.println("Content-Type: text/html; charset=utf-8");//version; answer code;
+    printWriter.println();
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("data/form.html"), Charset.forName("UTF8")));
+    while (true) {
+      String str = bufferedReader.readLine();
+      if (str == null) break;
+      printWriter.println(str);
+    }
+    printWriter.flush();
+  }
+  public static  void sendToBrowserOkForm(Socket connector)throws Exception {
+    PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(connector.getOutputStream(), Charset.forName("UTF-8")));
+    printWriter.println("HTTP/1.1 200 OK");//version; answer code;
+    printWriter.println("Content-Type: text/html; charset=utf-8");//version; answer code;
+    printWriter.println();
+    printWriter.println("<html><head></head><body<p>Sended!</p</body></html>");
     printWriter.flush();
   }
 
@@ -54,8 +97,19 @@ public class WebServerApp {
       Socket connector = listener.accept();
       readFromBrowser(connector);
       logBufferToFile();
-      if (buffer.get(0).contains("GET /index.html HTTP/1.1")) sendToBrowserOk(connector);
-      else sendToBrowserNotFound(connector);
+      if (browserRequestBuffer.get(0).contains("GET /123 HTTP/1.1")) {
+        sendToBrowserOk(connector);
+      } else if (browserRequestBuffer.get(0).contains("GET /index HTTP/1.1")) {
+        sendToBrowserOk(connector);
+      } else if (browserRequestBuffer.get(0).contains("GET /form.html HTTP/1.1")) {
+        sendToBrowserForm(connector);
+      } else if (browserRequestBuffer.get(0).contains("POST /form.html HTTP/1.1")) {
+        sendToBrowserOkForm(connector);
+      }
+      else {
+        sendToBrowserNotFound(connector);
+      }
+      browserRequestBuffer.clear();
       connector.close();
     }
 
