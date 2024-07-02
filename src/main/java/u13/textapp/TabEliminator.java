92d32e8bdcab53c6ae0ff3c74c29e9323974803a
GG <drcybfreeman@gmail.com>
2024-07-02 20:15:16 +0500

    TabEliminator task

---
+++ src/main/java/u13/textapp/TabEliminator.java
@@ -0,0 +1,28 @@
+package u13.textapp;
+/*
+A
+	B
+	C
+		D
+		E
+	F
+		G
+H
+I
+	J
+	K
+
+AB
+ACD
+ACE
+AFG
+H
+IJ
+IK
+
+ */
+public class TabEliminator {
+    public static void main(String[] args) {
+
+    }
+}
