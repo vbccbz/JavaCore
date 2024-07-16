fedab1ba66dae3818e2458271f613b168e7b0a29
GG <drcybfreeman@gmail.com>
2024-07-16 23:19:07 +0500

    upd

--- src/main/java/u9/annotation/AnnotationApp.java
+++ src/main/java/u9/annotation/AnnotationApp.java
@@ -5,10 +5,12 @@ import java.lang.annotation.Annotation;
 public class AnnotationApp {
     public static void main(String[] args) {
         try {
-            Class<?> classObject = Class.forName("java.lang.String");
+            Class<?> classObject = Class.forName("u9.annotation.Robot");
+            System.out.println(classObject.getName());
             for (Annotation annotation : classObject.getAnnotations()) {
                 System.out.println(annotation);
             }
+            System.out.println();
             for (var ctor : classObject.getConstructors()) {
                 System.out.println(ctor);
                 for (var annotation : ctor.getAnnotations()) {
@@ -16,6 +18,7 @@ public class AnnotationApp {
                 }
                 System.out.println();
             }
+            System.out.println("Check presenting of annotation:");
             if (classObject.getConstructor().isAnnotationPresent(MyAnnotation.class)) {
                 MyAnnotation annotation = classObject.getConstructor().getAnnotation(MyAnnotation.class);
                 System.out.println(annotation);
