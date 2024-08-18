a01d6f30ae3575528c879166bda3ed800ad64057
GG <drcybfreeman@gmail.com>
2024-08-18 20:07:12 +0500

    java javac

---
+++ src/main/java/u0/compile_and_linking/A.java
@@ -0,0 +1,8 @@
+package u0.compile_and_linking;
+
+public class A {
+  static String sa = "I am A";
+  public static void main (String[] args){
+    java.lang.System.out.println(sa);
+  }
+}
---
+++ src/main/java/u0/compile_and_linking/B.java
@@ -0,0 +1,9 @@
+package u0.compile_and_linking;
+
+public class B {
+  static String sb = "I am B";
+  public static void main (String[] args){
+    A a = new A();
+    java.lang.System.out.println(sb);
+  }
+}
