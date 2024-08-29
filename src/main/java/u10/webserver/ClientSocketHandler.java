91ddac681c4789c66dba9ab2efcb315bf482c239
GG <drcybfreeman@gmail.com>
2024-08-30 03:13:50 +0500

    big upd

--- src/main/java/u10/webserver/ClientSocketHandler.java
+++ src/main/java/u10/webserver/ClientSocketHandler.java
@@ -1,5 +1,7 @@
 package u10.webserver;
 
+import u2.homework.Participant;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -9,10 +11,11 @@ import java.nio.file.StandardOpenOption;
 
 public class ClientSocketHandler implements Runnable {
   private Socket socket;
-  private HTTPRequest httpRequest;
+  // private HTTPRequest httpRequest;
+  // private String page;
 
   @Override
-  public void run() {//throws Exception
+  public void run() {//what would happen if the handle throws an Exception?
     handle();
   }
 
@@ -22,82 +25,124 @@ public class ClientSocketHandler implements Runnable {
 
   public void handle() {
     try {
-      httpRequest = HTTPRequest.parse(socket);
-      log(Path.of("data/server_log.txt"), httpRequest.toString());
-      routing();
-      rendering();
+      HTTPRequest httpRequest = HTTPRequest.parse(socket);
+      log("data/server_log.txt", httpRequest.toString());
+      routing(httpRequest);
     } catch (Exception exception) {
       exception.printStackTrace();
     }
   }
 
-  public void log(Path path, String message) throws IOException {
-    if (Files.isRegularFile(path)) {
-      try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {//Charset.forName("UTF-8")
+  public void log(String path, String message) throws IOException {
+    Path path_to_log = Path.of(path);
+    if (Files.isRegularFile(path_to_log)) {
+      try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path_to_log, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {//Charset.forName("UTF-8")
         bufferedWriter.write(message);
         bufferedWriter.flush();
       }
     }
   }
 
-  public void routing() throws IOException {
+  public void routing(HTTPRequest httpRequest) throws IOException {
     String page;
-    if (httpRequest.method.equals("GET")) {
-      if (true) {
-        fetching();
-        // page.println("HTTP/1.1 200 OK");//version; answer code;
-        // page.println("Content-Type: text/html; charset=utf-8");//version; answer code;
-        // page.println();
-        rendering();
-      }
-    } else if (httpRequest.method.equals("POST")) {
-      mutation();
-      fetching();
-      // page.println("HTTP/1.1 303 See Other");//version; answer code;
-      // page.println("Location: /pform");//version; answer code;
-      // page.println();
-      rendering();
-
-      // page("HTTP/1.1 302 Found");//version; answer code;
-    } else {
-      fetching();//"404.html"
-      rendering();
-      //   page.println("HTTP/1.1 404 NOT_FOUND");
-      //   page.println("Content-Type: text/html; charset=utf-8");//version; answer code;
-      //   page.println();
+    switch (httpRequest.method) {
+      case "GET":
+        try {
+          page = fetching(httpRequest.path + ".html");
+          sendRespond(200, page);
+        } catch (IOException exception) {
+          try {
+            page = fetching("404.html");
+            sendRespond(404, page);
+          } catch (IOException exception2) {
+            sendRespond(505, "500 Internal Server Error");//kill server
+            sendRespond(505, "500 Internal Server Error");
+          }
+        }
+        break;
+      case "POST":
+        // mutation();
+        sendRespond(303,  httpRequest.path);
+        break;
+      default:
+        sendRespond(400, "400 Bad Request");
+        break;
     }
-
   }
 
-  public String fetching() throws IOException {
+  public String fetching(String path) throws IOException {
     StringBuilder stringBuilder = new StringBuilder();
-    Path path = Path.of(httpRequest.path);
-    if (Files.isRegularFile(path)) {
-      String currentLine = null;
-      // why does PrintWriter has SOO, but buffered doesn't?
-      try (BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-        while ((currentLine = bufferedReader.readLine()) != null) {
-          stringBuilder.append(currentLine);
-        }
+    String currentLine = null;
+    // why does PrintWriter has SOO, but buffered doesn't?
+    Path ppath = Path.of("data", path);
+    try (BufferedReader bufferedReader = Files.newBufferedReader(ppath, StandardCharsets.UTF_8)) {
+      while ((currentLine = bufferedReader.readLine()) != null) {
+        stringBuilder.append(currentLine);
+        stringBuilder.append('\n');
       }
     }
     return stringBuilder.toString();
   }
 
-  public void mutation() throws IOException {
-    Path path = Path.of(httpRequest.path);
-    if (Files.isRegularFile(path)) {
-      PrintWriter printWriter = new PrintWriter(Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND));
-      printWriter.write(httpRequest.body);
-      printWriter.println();
-      printWriter.close();
+  public void rendering() throws IOException {
+    // String page = fetching(httpRequest.path);
+    String base = fetching("data/messages.txt");
+
+  }
+
+  public void mutation(String path, String body) throws IOException {
+    try (PrintWriter printWriter = new PrintWriter(Files.newBufferedWriter(Path.of(path), StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND));) {
+      // PrintWriter printWriter1 = new PrintWriter(new FileWriter(path.toFile(), StandardCharsets.UTF_8, true));
+      // PrintWriter printWriter2 = new PrintWriter(path.toFile(),StandardCharsets.UTF_8);
+      printWriter.println("anonymous said: ");
+      printWriter.println(body);// if null - what to do?
     }
   }
 
-  public void rendering() throws IOException {
-    PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), false, StandardCharsets.UTF_8);
-    // toSocket.flush();
-    // bufferedReader.transferTo(toSocket);
+  public void sendRespond(int statusCode, String body) {
+    try (PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), false, StandardCharsets.UTF_8);) {
+      switch (statusCode) {
+        case 200:
+          printWriter.println("HTTP/1.1 200 OK");
+          printWriter.println("Content-Type: text/html; charset=utf-8");
+          printWriter.println("Content-Length: " + body.getBytes().length);
+          printWriter.println();
+          printWriter.println(body);
+          break;
+        case 303:
+          printWriter.println("HTTP/1.1 303 See Other");
+          printWriter.println("Content-Type: text/html; charset=utf-8");
+          printWriter.println("Location: " + body);
+          printWriter.println();
+          break;
+        case 400:
+          printWriter.println("HTTP/1.1 400 Bad Request");
+          printWriter.println("Content-Type: text/html; charset=utf-8");
+          printWriter.println("Content-Length: " + body.getBytes().length);
+          printWriter.println();
+          printWriter.println(body);
+          break;
+        case 404:
+          printWriter.println("HTTP/1.1 404 Not Found");
+          printWriter.println("Content-Type: text/html; charset=utf-8");
+          printWriter.println("Content-Length: " + body.getBytes().length);
+          printWriter.println();
+          printWriter.println(body);
+          break;
+        case 500:
+          printWriter.println("HTTP/1.1 500 Internal Server Error");
+          printWriter.println("Content-Type: text/html; charset=utf-8");
+          printWriter.println("Content-Length: " + body.getBytes().length);
+          printWriter.println();
+          printWriter.println(body);
+          break;
+        default:
+          throw new IllegalArgumentException();
+      }
+      printWriter.flush();
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
   }
 
 }
--- src/main/java/u10/webserver/WebServerApp.java
+++ src/main/java/u10/webserver/WebServerApp.java
@@ -5,24 +5,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class WebServerApp {
-  /*
-  SS -> S | TCPView | S | TCPView
-  "localhost" |
-  "127.0.0.1"
-  "192.168.1.7"
-  "0.0.0.0"
-   */
   public static void main(String[] args) throws Exception {
-    // ServerSocket listener = new ServerSocket(8081);
-    // ServerSocket listener = new ServerSocket(8081, 100, InetAddress.getByName("localhost"));
-    // ServerSocket listener = new ServerSocket(8081, 100, InetAddress.getByName("192.168.1.2"));
     ServerSocket listener = new ServerSocket();
     listener.bind(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), 8081), 100);//void bind(SocketAddress endpoint, int backlog)
-    ExecutorService executorService = Executors.newFixedThreadPool(2);
+    // ExecutorService executorService = Executors.newFixedThreadPool(2);
     while (true) {
       Socket socket = listener.accept();
       ClientSocketHandler clientSocketHandler = new ClientSocketHandler(socket);
-      executorService.execute(clientSocketHandler);
+      // executorService.execute(clientSocketHandler);
+      clientSocketHandler.handle();
     }
     // listener.close();
   }
